4e352efb1724523cc7d7df49319e0cf3bb88ebf2
giannurwana19 <giannurwana19@gmail.com>
2021-08-05 12:36:21 +0700

    basic routing, request & response

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 
 const app = express();
+const router = express.Router();
 const port = 4000;
 
-app.use(() => {
-  console.log('Hello server');
+router.use('/products', (req, res, next) => {
+  res.json({ name: 'Gian Nurwana', email: 'gian@gmail.com' });
+  next();
 });
+
+router.use('/price', (req, res, next) => {
+  res.json({ price: 150000 });
+  next();
+});
+
+router.get('/customers', (req, res, next) => {
+  res.json({ title: 'Customer' });
+  next();
+});
+
+app.use('/', router);
 
 app.listen(port);
