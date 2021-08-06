2ef1c0e5e4d66368fa7d848e7cc67596a9f80219
giannurwana19 <giannurwana19@gmail.com>
2021-08-06 14:49:20 +0700

    fix erros CORS POLICY

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const port = 4000;
 
 const productRoutes = require('./src/routes/products');
 
-app.use('/', productRoutes);
+// mencegah cors origin
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+// app.use(productRoutes);
+app.use('/v1/customer', productRoutes);
 
 app.listen(port);
+
+// h: DOCS
+// * artinya mengizinkan semua akses origin untuk masuk ke aplikasi
